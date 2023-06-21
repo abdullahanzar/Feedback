b6b54ecf30ed63fc03be7f8c9e293395a756d6b0
abdullahanzar <31632960+abdullahanzar@users.noreply.github.com>
2023-06-22 02:57:16 +0530

    Home Page Complete

--- Feedback/src/WebView/HomePage/Components/ProductCards.jsx
+++ Feedback/src/WebView/HomePage/Components/ProductCards.jsx
@@ -1,9 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import "./ProductCards.css";
+import commentImage from "./commentIcon.png";
+import commentButton from './commentButton.png'
 
 export default function ProductCards(props) {
   const [cards, setCards] = useState([]);
+  const [selectedCard, setSelectedCard] = useState(null);
   useEffect(() => {
     (async () =>
       setCards(await getCards(props.chosenCategory, props.sortBy)))();
@@ -12,7 +15,25 @@ export default function ProductCards(props) {
     const sorted = sortCards(cards, props.sortBy);
     setCards([...sorted]);
   }, [props.sortBy]);
-  return <>{displayCards(cards)}</>;
+  async function handleUpVote(companyName) {
+    try {
+      const upVote = await axios.post(
+        "https://feedback-d89u.onrender.com/upvote",
+        {
+          companyName: companyName,
+        },
+        {
+          headers: {
+            "content-type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      setCards(await getCards(props.chosenCategory, props.sortBy))();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return <>{displayCards(cards, handleUpVote)}</>;
 }
 
 async function getCards(category, sortBy) {
@@ -35,34 +56,35 @@ function sortCards(cards, sortBy) {
   }
 }
 
-function displayCards(cards) {
+function displayCards(cards, handleUpVote) {
   return (
     <div className="cards">
       {cards.map((item, key) => {
-        return (<>
+        return (
           <div className="card" key={key}>
             <img src={`${item.logoURL}`} alt={"image"} />
             <p>{item.companyName}</p>
             <p>{item.productDescrip}</p>
+            <div className="cardContainer">
             {item.category.map((item, key) => (
               <button key={key}>{item}</button>
             ))}
-            <button className="upvotes" onClick={()=>handleUpVote(item.companyName)}><span>^</span><span>{item.upVotes}</span></button>
+            </div>
+            <div className="commentButton"><img src={commentButton} alt="comment" />&nbsp;&nbsp;&nbsp;Comment</div>
+            <button
+              className="upvotes"
+              onClick={() => handleUpVote(item.companyName)}
+            >
+              <span>^</span>
+              <span>{item.upVotes}</span>
+            </button>
+            <div className="comment" onClick={()=>handleComment()}>
+              {item.comments.length}
+              <img src={commentImage} alt="Comment" />
+            </div>
           </div>
-          </>
         );
       })}
     </div>
   );
-}
-
-async function handleUpVote(companyName) {
-  try {
-    const upVote = await axios.post('https://feedback-d89u.onrender.com/upvote', {
-      companyName: companyName,
-    })
-    console.log(upVote)
-  } catch(e) {
-    console.log(e)
-  }
 }
